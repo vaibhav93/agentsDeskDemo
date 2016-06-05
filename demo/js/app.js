04d0da104e4388b16beacc4166d8b5e74512ec73
Vaibhav Bansal <vaibhav.b.bansal@oracle.com>
2016-06-05 17:08:31 +0530

    purge data above fixed

--- demo/js/app.js
+++ demo/js/app.js
@@ -1,23 +1,11 @@
-var app = angular.module('agentsDesk', ['ngTable', 'infinite-scroll'])
-    .controller('mainCtrl', function ($scope, $filter, ngTableParams, $http) {
+var app = angular.module('agentsDesk', ['bsLoadingOverlay'])
+    .controller('mainCtrl', function ($scope, $filter, $http, bsLoadingOverlayService) {
         $scope.page_size = 10;
         $scope.clients = [];
         $scope.data = [];
-        $scope.tableParams = new ngTableParams({
-            page: 1,
-            count: $scope.clients.length
-        }, {
-            counts: [],
-            total: 1,
-            getData: function ($defer, params) {
-                $scope.data = params.sorting() ? $filter('orderBy')($scope.clients, params.orderBy()) : $scope.clients;
-                $scope.data = params.filter() ? $filter('filter')($scope.data, params.filter()) : $scope.data;
-                if ($scope.data.length > 1)
-                    $scope.data = $scope.data.slice(0, 20);
-                $defer.resolve($scope.data);
-            }
-        });
-        var firstIndex,lastIndex;
+
+        var lastIndex;
+
         function filterByEmail(value) {
 
             if (value.emails && value.emails[0].email && value.emails[0].email.indexOf($scope.searchParam) > -1) {
@@ -37,35 +25,54 @@ var app = angular.module('agentsDesk', ['ngTable', 'infinite-scroll'])
         }
 
         $scope.getMoreData = function (direction) {
-            console.log(direction);
-            console.log('Getting Data');
-            if (direction){ //Going down. add data below
-                $scope.clients.push.apply($scope.clients, $scope.data.slice(lastIndex+1, lastIndex + $scope.page_size*3+1));
-                lastIndex = lastIndex + $scope.page_size*3;
-            }
-            else{  //scroll up
-//                $scope.clients.push.apply($scope.clients, $scope.data.slice($scope.clients.length, $scope.page_size));
+            //            console.log(direction);
+            //            console.log('Getting Data');
+            if (direction) { //Going down. add data below
+                console.log("length before adding:" + $scope.clients.length);
+                $scope.clients.push.apply($scope.clients, $scope.data.slice(lastIndex + 1, lastIndex + $scope.page_size * 3 + 1));
+                console.log("length after adding:" + $scope.clients.length);
+                lastIndex = lastIndex + $scope.page_size * 3;
+            } else { //scroll up
+                if ($scope.firstIndex > 0) {
+                    var start = $scope.firstIndex - $scope.page_size * 7
+                    if (start < 0)
+                        start = 0;
+                    console.log("start at:" + start + " end at " + $scope.firstIndex);
+                    // console.log($scope.data);
+                    $scope.clients.unshift.apply($scope.clients, $scope.data.slice(start, $scope.firstIndex));
+                    $scope.firstIndex = $scope.firstIndex - $scope.page_size * 7;
+                    console.log("First Index:" + $scope.firstIndex);
+                    if ($scope.firstIndex < 0)
+                        $scope.firstIndex = 0;
+                }
             }
 
         }
-        $scope.purgeData = function (direction) {
-            console.log('Purging Data');
-            if (!direction){ //going down. Delete rows on top
-                console.log("Length before purging: "+$scope.clients.length);
-                $scope.clients.splice(lastIndex-$scope.page_size+1, $scope.page_size);
-                lastIndex = lastIndex - $scope.page_size;
-                console.log("Length after purging: "+$scope.clients.length);
+        $scope.purgeData = function (direction, rows) {
+
+            if (direction) { //going down. Delete rows on top
+                console.log($scope.clients.length);
+                $scope.clients.splice(0, $scope.page_size * 10);
+                $scope.firstIndex = $scope.firstIndex + $scope.page_size * 10;
+                console.log("first index:" + $scope.firstIndex);
+                //                console.log($scope.clients);
+                console.log("purgin ablove");
+            } else {
+                //                console.log("Length before purging: "+$scope.clients.length);
+                $scope.clients.splice(lastIndex - $scope.page_size * 3 + 1, $scope.page_size * 3);
+                lastIndex = lastIndex - $scope.page_size * 3;
+                //                console.log("Length after purging: "+$scope.clients.length);
             }
-//                $scope.clients.splice(0,$scope.page_size);
+            //                $scope.clients.splice(0,$scope.page_size);
         }
 
         var getData = function () {
             $http.get('js/clients.json').success(function (data) {
                 $scope.data = data.clients;
-                
-                $scope.clients = data.clients.splice(0, 100);
-                firstIndex = 0;
-                lastIndex = $scope.clients.length - 1 ;
+                $scope.clients = data.clients.slice(0, 501);
+                console.log("Length:" + $scope.clients.length);
+                $scope.firstIndex = 0;
+                lastIndex = $scope.clients.length - 1;
             });
         }
         getData();
--- demo/js/directives/infiniteScroll.js
+++ demo/js/directives/infiniteScroll.js
@@ -1,28 +1,46 @@
-app.directive('infinite', function ($window, $timeout) {
+app.directive('infinite', function ($window, $timeout, bsLoadingOverlayService) {
     return {
         scope: {
             getData: '&',
             purgeData: '&',
-            pageSize: '='
+            pageSize: '=',
+            firstIndex: '='
         },
         restrict: 'A',
         link: function (scope, elem, attrs, window) {
             var PAGE_SIZE = scope.pageSize;
             var windowElement = angular.element($window);
-            var scrollDelay = 250,
+            var scrollDelay = 300,
                 scrollThrottleTimeout,
                 throttled = false,
-                scrollTop,   //Current scrolled
+                scrollTop, //Current scrolled
                 elementTopOffset, //Table offset from top
                 rowHeight;
-            var pixelsAboveCurrentTop, pixelsBelowCurrentBottom;  //Table pixels
-            var rowsAbove,rowsBelow;
-            var pagesAbove,pagesBelow;
-            var getRowsFromTotalHeight = function(totalHeight,rowHeight){
-                return Math.round(totalHeight/rowHeight);
+            var pixelsAboveCurrentTop, pixelsBelowCurrentBottom; //Table pixels
+            var rowsAbove, rowsBelow;
+            var pagesAbove, pagesBelow;
+            var getRowsFromTotalHeight = function (totalHeight, rowHeight) {
+                return Math.round(totalHeight / rowHeight);
             }
-            var getPagesFromRows = function(page_size,rows){
-                return Math.floor(rows/page_size);
+            var purgedOnce = false;
+            var getPagesFromRows = function (page_size, rows) {
+                return Math.floor(rows / page_size);
+            }
+            var fired = false;
+            var calculate = function () {
+                scrollTop = windowElement.scrollTop();
+                pixelsAboveCurrentTop = scrollTop - elementTopOffset;
+                pixelsBelowCurrentBottom = elementTopOffset + elem.height() - (scrollTop + windowElement.height());
+                if (pixelsAboveCurrentTop > 0) {
+                    rowsAbove = getRowsFromTotalHeight(pixelsAboveCurrentTop, rowHeight);
+                    pagesAbove = getPagesFromRows(PAGE_SIZE, rowsAbove);
+                    console.log("pages Above , rows above: " + pagesAbove + "," + rowsAbove);
+                }
+                if (pixelsBelowCurrentBottom > 0) {
+                    rowsBelow = getRowsFromTotalHeight(pixelsBelowCurrentBottom, rowHeight);
+                    pagesBelow = getPagesFromRows(PAGE_SIZE, rowsBelow);
+                    //                    console.log("pages below , rows below: " + pagesBelow + "," + rowsBelow);
+                }
             }
             $timeout(function () {
                 elementTopOffset = elem.offset().top;
@@ -31,42 +49,62 @@ app.directive('infinite', function ($window, $timeout) {
                 console.log("Element height: " + elem.height());
                 console.log("Element tag: " + elem.prop("tagName"));
                 console.log("Row Height " + elem.find("tr").height());
+                calculate();
             }, 500);
+
+
             var scrollHandler = function () {
-                scrollTop = windowElement.scrollTop();
-                pixelsAboveCurrentTop = scrollTop - elementTopOffset;
-                pixelsBelowCurrentBottom = elementTopOffset + elem.height() - (scrollTop + windowElement.height());
-                if(pixelsAboveCurrentTop>0){
-                    rowsAbove = getRowsFromTotalHeight(pixelsAboveCurrentTop,rowHeight);
-                    pagesAbove = getPagesFromRows(PAGE_SIZE,rowsAbove);
-                    console.log("pages Above , rows above: " + pagesAbove+","+rowsAbove);
-                    if(pagesAbove>=3){
-                        
-                        //remove excess from list
-                        //scope.purgeData({value:true});
-                       // windowElement.scrollTop(scrollTop - PAGE_SIZE*rowHeight);
-                    } else if(pagesAbove<=2){
-                        //load more data
-                       // scope.getData({value:false});
-                    }
+                calculate();
+                if (pagesBelow > 15) {
+                    //                    remove excess from list
+                    console.log("Purge Data now");
+                    scope.purgeData({
+                        value: false,
+                        rows: null
+                    });
+                } else if (pagesBelow <= 5) {
+                    console.log("Get Data now");
+                    scope.getData({
+                        value: true
+                    });
                 }
-                if(pixelsBelowCurrentBottom>0){
-                    rowsBelow = getRowsFromTotalHeight(pixelsBelowCurrentBottom,rowHeight);
-                    pagesBelow = getPagesFromRows(PAGE_SIZE,rowsBelow);
-                    if(pagesBelow>10){
-                        //remove excess from list
-//                        console.log("Purge Data now");
-                        scope.purgeData({value:false});
-                    } else if(pagesBelow<=5){
-                        //load more data
-                        console.log("Get Data now");
-                        scope.getData({value:true});
+                if (pagesAbove > 30 || (pagesAbove < 4 && purgedOnce && scope.firstIndex > 0)) {
+
+                    if (!fired) {
+                        angular.element(document.querySelector('body')).addClass('stop-scrolling');
+                        fired = true;
+                        bsLoadingOverlayService.start({
+                            referenceId: 'first'
+                        });
+                        if (pagesAbove > 30) {
+                            scope.purgeData({
+                                value: true,
+                                rows: null
+                            })
+                            windowElement.scrollTop(scrollTop - rowHeight * PAGE_SIZE * 10);
+                            purgedOnce = true;
+                        } else if (pagesAbove < 4 && purgedOnce && scope.firstIndex > 0) {
+                            scope.getData({
+                                value: false
+                            });
+                            windowElement.scrollTop(scrollTop + rowHeight * PAGE_SIZE * 7);
+                        }
+
+                        setTimeout(function () {
+                            angular.element(document.querySelector('body')).removeClass('stop-scrolling');
+                            bsLoadingOverlayService.stop({
+
+                                referenceId: 'first'
+                            });
+                            fired = false;
+                        }, 2500);
+
                     }
+
                 }
-                    
-//                
-//                console.log("pages Below: " + pagesBelow);
             }
+
+            //requestAnimationFrame(repeatOften);
             var throttleScroll = function () {
                 if (!throttled) {
                     scrollHandler();
